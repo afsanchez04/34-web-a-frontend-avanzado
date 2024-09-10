9cefb0aa59466575fed9a47878755b28efda0489
afsaanchez04 <afsanchez04@gmail.com>
2024-09-09 23:22:28 -0500

    sass

---
+++ intro-frontend-avanzado/src/App.jsx
@@ -0,0 +1,20 @@
+//rafc (snippet)
+
+export const App = () => {
+    return (
+        <>
+            <section className="container">
+                <div>
+                    <h1>Tienda de discos</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, maxime.</p>
+                    <button className="btn">Comprar</button>
+                    <div>
+                        <button className="btn-sumar">Sumar</button>
+                        <button className="btn-restar">Restar</button>
+                        <button className="btn-resetear">Resetear</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
