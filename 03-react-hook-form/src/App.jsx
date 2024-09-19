367268cd871fc0799ecf6c5f26eff51648cd1d73
afsaanchez04 <afsanchez04@gmail.com>
2024-09-18 21:13:07 -0500

    formulario-forma-controlada

--- 03-react-hook-form/src/App.jsx
+++ 03-react-hook-form/src/App.jsx
@@ -1,21 +1,60 @@
-import { useRef } from "react"
+import { useState } from "react"
 
 export const App = () => {
 
+  const initialForm = {
+    nombre: '',
+    correo: '',
+    password: '',
+    colores: ''
+  }
+
+  const [formState, setFormState] = useState(initialForm)
+
+  const handleInputChange = ({target}) => {
+    const {name, value} = target
+    setFormState( {
+      ...formState,
+      [name]: value
+    } )
+
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    const inputs = Object.fromEntries( new FormData(event.target) ) 
-    // const nombre = inputs.get('nombre')
-    // const correo = inputs.get('correo')
-    console.log(inputs)
-  } 
+    console.log(formState)
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name="nombre" placeholder="Nombre..." type="text" />
-      <input name="correo" placeholder="Correo..." type="email" />
-      <input name="password" placeholder="Contraseña..." type="password" />
-      <select name="colores">
+      <input 
+        name="nombre" 
+        placeholder="Nombre..." 
+        type="text"
+        onChange={handleInputChange} 
+      />
+      <input 
+        name="pais" 
+        placeholder="País..." 
+        type="text"
+        onChange={handleInputChange} 
+      />
+      <input 
+        name="correo" 
+        placeholder="Correo..." 
+        type="email"
+        onChange={handleInputChange} 
+      />
+      <input 
+        name="password" 
+        placeholder="Contraseña..." 
+        type="password"
+        onChange={handleInputChange} 
+      />
+      <select 
+        name="colores"
+        onChange={handleInputChange}
+      >
         <option value="red">Rojo</option>
         <option value="blue">Blue</option>
         <option value="yellow">Yellow</option>
