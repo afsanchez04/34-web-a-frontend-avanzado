8ae819c6d26c48b41a1f0ce26d8633d661f0ae88
afsaanchez04 <afsanchez04@gmail.com>
2024-09-18 20:42:23 -0500

    formulario-forma-no-controlada

--- 03-react-hook-form/src/App.jsx
+++ 03-react-hook-form/src/App.jsx
@@ -2,19 +2,25 @@ import { useRef } from "react"
 
 export const App = () => {
 
-  const inputNombre = useRef()
-  const inputEmail = useRef()
-
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(inputNombre.current.value)
-    console.log(inputEmail.current.value)
+    const inputs = Object.fromEntries( new FormData(event.target) ) 
+    // const nombre = inputs.get('nombre')
+    // const correo = inputs.get('correo')
+    console.log(inputs)
   } 
 
   return (
     <form onSubmit={handleSubmit}>
-      <input ref={inputNombre} name="nombre" placeholder="Nombre..." type="text" />
-      <input ref={inputEmail} name="correo" placeholder="Correo..." type="email" />
+      <input name="nombre" placeholder="Nombre..." type="text" />
+      <input name="correo" placeholder="Correo..." type="email" />
+      <input name="password" placeholder="Contraseña..." type="password" />
+      <select name="colores">
+        <option value="red">Rojo</option>
+        <option value="blue">Blue</option>
+        <option value="yellow">Yellow</option>
+        <option value="tomato">Tomato</option>
+      </select>
       <button type="submit">Enviar</button>
     </form>
   )
