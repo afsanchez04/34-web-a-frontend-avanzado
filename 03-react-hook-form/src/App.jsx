d4b0ba308350b30ca133b3c32c2a7dc46fad33f4
afsaanchez04 <afsanchez04@gmail.com>
2024-09-18 23:02:22 -0500

    react-hook-form y ejercicios

--- 03-react-hook-form/src/App.jsx
+++ 03-react-hook-form/src/App.jsx
@@ -1,56 +1,96 @@
-import { useFormulario } from "./components/useFormulario"
+import { useForm } from "react-hook-form"
 
 export const App = () => {
 
-  const initialForm = {
-    nombre: '',
-    correo: '',
-    password: '',
-    colores: ''
-  }
+  const { register, handleSubmit, formState: {errors}, watch, reset } = useForm()
 
-  const {formState, handleInputChange} = useFormulario(initialForm)
+  const onSubmit = handleSubmit( data => {
+    console.log(data)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(formState)
-  }
+    reset()
+  } )
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input 
-        name="nombre" 
-        placeholder="Nombre..." 
+    <form onSubmit={onSubmit}>
+      <input
+        name="nombre"
+        placeholder="Nombre..."
         type="text"
-        onChange={handleInputChange} 
+        {...register("nombre",{
+          required: 'Nombre es requerido',
+          maxLength: {
+            value: 5,
+            message: 'Máximo 6 caracteres!'
+          }
+        })}
       />
-      <input 
-        name="pais" 
-        placeholder="País..." 
-        type="text"
-        onChange={handleInputChange} 
-      />
-      <input 
-        name="correo" 
-        placeholder="Correo..." 
+      {errors.nombre && <small className="text-danger">{errors.nombre.message}</small>}
+      <input
+        name="correo"
+        placeholder="Correo..."
         type="email"
-        onChange={handleInputChange} 
+        {...register("correo",{
+          required: 'Correo es requerido'
+        })}
       />
-      <input 
-        name="password" 
-        placeholder="Contraseña..." 
+      <input
+        name="password"
+        placeholder="Contraseña..."
         type="password"
-        onChange={handleInputChange} 
+        {...register("password",{
+          required: 'Contraseña es requerida',
+          minLength: {
+            value: 5,
+            message: 'Contraseña debe tener mínimo 5 caracteres'
+          }
+        })}
       />
-      <select 
-        name="colores"
-        onChange={handleInputChange}
+      {errors.password && <small className="text-danger">{errors.password.message}</small>}
+      <input
+        name="password"
+        placeholder="Confirmar contraseña..."
+        type="password"
+        {...register("confirmarPassword",{
+          required: 'Debes confirmar tu contraseña',
+          minLength: {
+            value: 5,
+            message: 'Contraseña debe tener mínimo 5 caracteres'
+          },
+          validate: (value) => {
+            return value === watch('password') || "Las contraseñas no coinciden"
+          }
+        })}
+      />
+      {errors.confirmarPassword && <small className="text-danger">{errors.confirmarPassword.message}</small>}
+      <select
+        name="paises"
+        {...register("paises")}
       >
-        <option value="red">Rojo</option>
-        <option value="blue">Blue</option>
-        <option value="yellow">Yellow</option>
-        <option value="tomato">Tomato</option>
+        <option value="colombia">Colombia</option>
+        <option value="mexico">Mexico</option>
+        <option value="chile">Chile</option>
+        <option value="peru">Peru</option>
       </select>
+
+      {
+        watch('paises') === 'colombia' && (
+          <input {...register('departamento',{required: true})} type="text" placeholder="Departamento..."/>
+        )
+      }
+
+      <div className="form-check">
+        <input 
+          name="terminos" 
+          className="form-check-input" 
+          type="checkbox" 
+          value=""
+          {...register("terminos")}
+        />
+          <label className="form-check-label" htmlFor="flexCheckDefault" >
+            Aceptar términos
+          </label>
+      </div>
+
       <button type="submit">Enviar</button>
     </form>
   )
---
+++ 03-react-hook-form/src/ejercicios/ejercicios-formularios.md
@@ -0,0 +1,33 @@
+
+### Ejercicio 1: Validación de un formulario de registro de usuario
+
+Crear un formulario de registro de usuario que valide los siguientes campos:
+
+- Nombre (debe tener al menos 3 caracteres)
+- Email (formato correcto de correo electrónico)
+- Contraseña (mínimo 6 caracteres)
+
+
+### Requisitos:
+
+- Si algún campo no cumple con la validación, debe mostrarse un mensaje de error debajo del campo correspondiente.
+- Mostrar en consola los datos del formulario si todos los campos son válidos.
+
+
+
+### Ejercicio 2: Formulario de contacto con validación personalizada
+
+Crear un formulario de contacto que incluya:
+
+- Nombre completo (debe tener al menos 5 caracteres).
+- Mensaje (debe tener entre 10 y 300 caracteres).
+- Campo de edad (debe ser mayor de 18 años).
+
+### Requisitos:
+
+- Mostrar mensajes de error específicos para cada validación fallida.
+- Al enviar correctamente el formulario, mostrar los datos en consola.
+- Agregar un estado de "éxito" cuando el formulario sea válido y enviado.
+- Usar estilos básicos con Bootstrap para que los formularios sean más atractivos visualmente.
+
+
